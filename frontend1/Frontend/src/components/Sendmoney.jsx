669cc876a9b1f2c86eab619282c2406f335b6f7e
pavan <pavanraghavendra1963@gmail.com>
2024-02-10 12:42:51 +0530

    Final update

--- frontend1/Frontend/src/components/Sendmoney.jsx
+++ frontend1/Frontend/src/components/Sendmoney.jsx
@@ -1,4 +1,4 @@
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import axios from "axios";
 import { useState } from 'react';
 
@@ -7,7 +7,7 @@ export const SendMoney = () => {
     const id = searchParams.get("id");
     const name = searchParams.get("name");
     const [amount, setAmount] = useState(0);
-
+    const navigation=useNavigate();
     return <div className="flex justify-center h-screen bg-gray-100">
         <div className="h-full flex flex-col justify-center">
             <div
@@ -40,15 +40,26 @@ export const SendMoney = () => {
                         placeholder="Enter amount"
                     />
                     </div>
-                    <button onClick={() => {
-                        axios.post("http://localhost:3001/api/accountdata/transfer", {
+                    <button onClick={async () => {
+                       const response= await axios.post("http://localhost:3001/api/accountdata/transfer", {
                             to: id,
                             amount
                         }, {
                             headers: {
                                 Authorization: localStorage.getItem("token")
                             }
-                        })
+                        });
+                        if(response.data.message==="Transfer successful")
+                        {
+                            setTimeout(()=>
+                            {
+                                alert("Transfer Successful");
+                            },2000)
+                            navigation("/dashboard");
+                        }
+                        else{
+                            alert("Transfer Unsuccessful");
+                        }
                     }} className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white">
                         Initiate Transfer
                     </button>
