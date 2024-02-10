a476bd87d283e7349bca31b32fa152899b7432bd
pavan <pavanraghavendra1963@gmail.com>
2024-02-10 17:13:56 +0530

    Up

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
 const express=require("express");
 const cors=require("cors");
 const app=express();
-app.use(cors());
+app.use(cors(
+    {
+        origin:["https://money-wallet-app.vercel.app/"],
+        methods:["POST","GET"],
+        credentials:true
+    }
+));
 const Mainrouter=require("./routes/routes")
 app.use(express.json());
 app.use("/api",Mainrouter);
